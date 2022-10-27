ab7b7ce99ca1aa91822b89bcb78d73f7e1d661c8
Bikash Adhikari <bikash30851@gmail.com>
2022-10-26 19:34:34 -0500

    update header button

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -6,7 +6,7 @@ const HeaderComponent = () => {
         <header>
             <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
                 <div>
-                    <a href='https://javaguides.net' className='navbar-brand'> 
+                    <a target="_blank" rel="noopener noreferrer" href='https://bikashad.com/' className='navbar-brand'> 
                         Employee Management Application
                     </a>
                 </div>
